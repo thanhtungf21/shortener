d4b3fd7426a7f39c1d9598a560c279ba00943e2d
thanhtungf2 <thanhtungf21@gmail.com>
2025-06-22 01:11:31 +0700

    shortener(feat): update config db

--- config/db.js
+++ config/db.js
@@ -3,12 +3,12 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        // Xóa object chứa các option cũ đi
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
+        // Dòng này rất quan trọng để in ra lỗi kết nối như bạn thấy
         console.error(`Error connecting to MongoDB: ${error.message}`);
         process.exit(1);
     }
